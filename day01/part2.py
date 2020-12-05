da06dcf64f28fa7597ba3f8654da872f8e23d40d
Marco Sirabella <marco@sirabella.org>
2020-12-04 18:47:08 -0800

    Add part2 with same algorithm as part1

---
+++ day01/part2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+import fileinput
+from bisect import insort, bisect, bisect_right
+
+numlist = [int(line) for line in fileinput.input()]
+
+
+def part2(l):
+    l = sorted(l)
+    while l:
+        if l[0] + l[-1] + l[-2] == 2020:
+            return l[0] * l[-1] * l[-2]
+        if l[0] + l[-1] + l[1] == 2020:
+            return l[0] * l[-1] * l[1]
+
+        l = l[:bisect(l, 2020 - (l[0] + l[1]))]
+        l = l[bisect(l, 2019 - (l[-1] + l[-2])):]
+
+
+print(part2(numlist))
