ab617c80ecb69fee7cf1870523f9778bc5418a41
Marco Sirabella <marco@sirabella.org>
2020-12-23 12:36:16 -0800

    Move processing into main loop
    This begins my changes based off of @ncfavier's code at:
    https://github.com/ncfavier/aoc/blob/master/src/2020/Day23.c
    
    Even though our business logic looks similar, theirs still half a second
    faster. Will be optimizing by making more similar to theirs, and then
    optimizing further.

--- day23/part2.c
+++ day23/part2.c
@@ -52,35 +52,6 @@ int modulo(int x,int N){
     return (x % N + N) %N;
 }
 
-circ_list *move(circ_list *cups) {
-	int current = cups->list->data;
-	// printf("cups: "); print_circ_list(*cups);
-	// printf("current: %i\n", current);
-	struct link *removed = cups->list->next; // three long
-	// printf("pick up: %i, %i, %i\n", removed->data, removed->next->data, removed->next->next->data);
-	cups->list->next = removed->next->next->next;
-	cups->list = cups->list->next;
-
-	int dest = current;
-	do {
-		dest = modulo(dest - 1, cups->size + 1);
-		if (dest == 0) {
-			dest = modulo(dest - 1, cups->size + 1);
-		}
-	} while (
-		   dest == removed->data
-		|| dest == removed->next->data
-		|| dest == removed->next->next->data
-	);
-	// printf("dest: %i\n", dest);
-	struct link *dest_p = cups->lookup[dest];
-
-	// do the move
-	removed->next->next->next = dest_p->next;
-	dest_p->next = removed;
-	return cups;
-}
-
 #define lenof(a) sizeof(a) / sizeof(a[0])
 int main(int argc, char *argv[]) {
 	int input[1000000] = {5, 6, 2, 8, 9, 3, 1, 4, 7};
@@ -88,8 +59,34 @@ int main(int argc, char *argv[]) {
 		input[i] = i + 1;
 	}
 	circ_list list = new_circ_list(input, lenof(input));
+	circ_list *cups = &list;
+
 	for (int i = 0; i < 10000000; i++) {
-		move(&list);
+		int current = cups->list->data;
+		// printf("cups: "); print_circ_list(*cups);
+		// printf("current: %i\n", current);
+		struct link *removed = cups->list->next; // three long
+		// printf("pick up: %i, %i, %i\n", removed->data, removed->next->data, removed->next->next->data);
+		cups->list->next = removed->next->next->next;
+		cups->list = cups->list->next;
+
+		int dest = current;
+		do {
+			dest = modulo(dest - 1, cups->size + 1);
+			if (dest == 0) {
+				dest = modulo(dest - 1, cups->size + 1);
+			}
+		} while (
+				 dest == removed->data
+			|| dest == removed->next->data
+			|| dest == removed->next->next->data
+		);
+		// printf("dest: %i\n", dest);
+		struct link *dest_p = cups->lookup[dest];
+
+		// do the move
+		removed->next->next->next = dest_p->next;
+		dest_p->next = removed;
 	}
 	printf("%li\n", (long int) list.lookup[1]->next->data * list.lookup[1]->next->next->data);
 }
